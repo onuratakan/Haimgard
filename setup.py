6d43c8b0167405bfb0ecbb9dee3a3bfd52420e6d
onuratakan <atadogan06@gmail.com>
2021-10-30 11:58:06 +0000

    Versin and requirements manualy aded to setup

--- setup.py
+++ setup.py
@@ -1,21 +1,9 @@
 from setuptools import setup
 import pathlib
 
-
-version_path = pathlib.Path.cwd() / "src" / "haimgard" / "VERSION.txt"
-with open(version_path, 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-    version = lines[0]
-
-requirements_path = pathlib.Path.cwd() / "requirements.txt"
-with open(requirements_path, 'r', encoding='utf-8') as f:
-    requirements = []
-    for requirement_line in f.readlines():
-        requirements.append(requirement_line.strip("\n"))
-
     
 setup(name='haimgard',
-version=version,
+version="0.2.1",
 description="""Haimgard is an environment for writing, testing and using exploit code.""",
 long_description="""
 # Haimgard-Framework
@@ -37,7 +25,13 @@ package_dir={'':'src'},
 package_data={
     "haimgard": ["modules/*/*.py", "*.txt"],
 },
-install_requires=requirements,
+install_requires="""
+commonmark==0.9.1
+cowsay==4.0
+loguru==0.5.3
+rich==10.12.0
+scapy==2.4.5
+""",
 entry_points = {
     'console_scripts': ['haimgard=haimgard.haimgard:main'],
 },
