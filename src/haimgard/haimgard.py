59244aab4adc0b0674e22d06739fe18e4c64e81c
Onur Atakan ULUSOY <atadogan06@gmail.com>
2021-12-05 12:06:24 +0000

    Added a control point for none or default value in modules

--- src/haimgard/haimgard.py
+++ src/haimgard/haimgard.py
@@ -122,7 +122,8 @@ class HaimgardShell(cmd.Cmd):
             )
             for option in self.options:
                 if option in self.module.options:
-                    self.module.options[option]["value"] = self.options[option]["value"]
+                    if not self.module.options[option]["value"] is None:
+                        self.module.options[option]["value"] = self.options[option]["value"]
         else:
             logger.error("Module already selected")
 
